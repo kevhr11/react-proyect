f3be228f1c48fcee7441a34aa10db2c881a2f5ae
Abisai <109362463+AbisaiMelendez@users.noreply.github.com>
2022-12-22 17:11:31 -0600

    succes react project last updater

--- src/components/containers/BussinesProfileHome/BussinesProfileHome.jsx
+++ src/components/containers/BussinesProfileHome/BussinesProfileHome.jsx
@@ -127,14 +127,7 @@ const BussinesProfileHome = () => {
             <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
               <h4 className="text-xl text-gray-900 font-bold">Description</h4>
               <p className="mt-2 text-gray-700">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Nesciunt voluptates obcaecati numquam error et ut fugiat
-                asperiores. Sunt nulla ad incidunt laboriosam, laudantium est
-                unde natus cum numquam, neque facere. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Ut, magni odio magnam
-                commodi sunt ipsum eum! Voluptas eveniet aperiam at maxime,
-                iste id dicta autem odio laudantium eligendi commodi
-                distinctio!
+              Somos una agencia de viajes regional que te ofrece convenios exclusivos para tu beneficio. Agencia de Viajes con certificacion mundial IATA, somos garantía de servicio y confianza. Asesoría profesional. Destinos Internacionales. Tu Agencia Confiable. Atención 24/7.
               </p>
             </div>
 
